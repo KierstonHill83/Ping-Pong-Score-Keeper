34ef21866fe3b76be84396e10c8ad57d742a879d
KierstonHill83 <khill1218@gmail.com>
2015-09-16 12:59:46 -0600

    Fixed the serving message

--- js/main.js
+++ js/main.js
@@ -1,8 +1,52 @@
 var app = angular.module('myApp', []);
 
 app.controller('pingPong', ['$scope', function($scope) {
+
+    $scope.count = 0;
+    $scope.count2 = 0;
+
   $scope.reset = function() {
     $scope.count = 0;
     $scope.count2 = 0;
+    $scope.message1 = "";
+    $scope.message2 = "";
   };
+
+  $scope.serve = function() {
+    if ($scope.count + $scope.count2 < 2) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 4) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 6) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 8) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 10) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 12) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 14) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 16) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 18) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    } else if ($scope.count + $scope.count2 < 20) {
+      $scope.message2 = "Serving";
+      $scope.message1 = "";
+    } else if ($scope.count + $scope.count2 < 22) {
+      $scope.message1 = "Serving";
+      $scope.message2 = "";
+    }
+  };
+
 }]);
